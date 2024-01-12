84f39f0f884536828463797a0abc74bba7d1a4bb
Rashidul <aboutrashidul@gmail.com>
2024-01-12 12:21:26 +0600

    LOGIN API Created

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,45 +1,6 @@
-// const userModel = require("../models/userModel");
-// const bcrypt = require("bcryptjs");
-
-// const registerController = async (req, res) => {
-//   try {
-//     const exisitingUser = await userModel.findOne({ email: req.body.email }); //validation
-//     //validation
-//     if (exisitingUser) {
-//       return res.status(200).send({
-//         success: false,
-//         message: "User ALready exists",
-//       });
-//     }
-
-//     //hash password
-//     const solt = await bcrypt.genSalt(10);
-//     const hashedPassword = await bcrypt.hash(req.body.password, solt);
-//     req.body.password = hashedPassword;
-
-//     //rest data
-
-//     const user = new userModel(req.body);
-//     await user.save();
-//     return res.ststus(201).send({
-//       success: true,
-//       message: "User Registerd Successfully",
-//       user,
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     res.ststus(500).send({
-//       success: false,
-//       message: "Error In Register API",
-//       error,
-//     });
-//   }
-// };
-
-// module.exports = { registerController };
-
 const userModel = require("../models/userModel");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const registerController = async (req, res) => {
   try {
@@ -77,4 +38,45 @@ const registerController = async (req, res) => {
   }
 };
 
-module.exports = { registerController };
+//login call back
+const loginController = async (req, res) => {
+  try {
+    const user = await userModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "User Not Found",
+      });
+    }
+    //compare password
+    const comparePassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!comparePassword) {
+      return res.status(500).send({
+        success: false,
+        message: "Invalid Credentials",
+      });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+      expiresIn: "1d",
+    });
+    return res.status(200).send({
+      success: true,
+      message: "Login Successfully",
+      token,
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error In Login API",
+      error,
+    });
+  }
+};
+
+module.exports = { registerController, loginController };
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { registerController } = require("../controllers/authController");
+const {
+  registerController,
+  loginController,
+} = require("../controllers/authController");
 
 const router = express.Router();
 
 //routes
 //REGISTER || POST
-
 router.post("/register", registerController);
+
+//LOGIN || POST
+router.post("/logn", loginController);
 module.exports = router;
