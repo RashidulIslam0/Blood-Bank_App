fe3ef94844f240f32b91118b9cf58677493302bf
Rashidul <aboutrashidul@gmail.com>
2024-01-12 21:38:32 +0600

    Reusable Register  From

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -4,7 +4,7 @@ import Form from "../../components/shared/Form/Form";
 const Login = () => {
   return (
     <div>
-      <div className="row">
+      <div className="row g-0">
         <div className="col-md-8 form-banner">
           <img src="./assets/images/banner1.jpg" alt="loginImage" />
         </div>
