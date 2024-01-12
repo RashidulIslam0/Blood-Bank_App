b6a3a12c037dce8d8b6984cb2bc551b4a4aa770c
Rashidul <aboutrashidul@gmail.com>
2024-01-12 11:29:03 +0600

    Project Setup

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+// const mongoose = require("mongoose");
+const morgan = require("morgan");
+require("dotenv").config();
+
+const connectDB = require("./DB/db");
+const app = express();
+app.use(morgan("dev"));
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Wordl");
+});
+app.get("*", (req, res) => {
+  res.status(404).send("<h1>404 Not found</h1>");
+});
+
+app.listen(PORT, async () => {
+  console.log(`server is running at http://localhost:${PORT}`);
+  await connectDB();
+});
